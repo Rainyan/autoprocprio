d7513a2a8bf92cd24fffa08f710a6749bb6779e4
Rain <Rainyan@users.noreply.github.com>
2023-09-23 17:43:11 +0300

    PLEASE

--- autoprocprio.py
+++ autoprocprio.py
@@ -45,7 +45,7 @@ if platform_is_windows():
     import win32api  # For catching user closing the app window via the X icon
 
 SCRIPT_NAME = "AutoProcPrio"
-SCRIPT_VERSION = "8.1.0"
+SCRIPT_VERSION = "8.1.1"
 
 
 def add_app(executable_name):
